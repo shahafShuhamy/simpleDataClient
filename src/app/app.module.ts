e489a58615dcb8936bff9c057bf202faab8abbfc
shahafShuhamy <Shahaf761@gmail.com>
2019-08-14 09:48:21 +0300

    Ui update orange theme

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopNavComponent } from './top-nav/top-nav.component';
+import { DataComponent } from './data/data.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopNavComponent,
+    DataComponent
   ],
   imports: [
     BrowserModule,
